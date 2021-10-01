8c89c946e2f006c806cd3d623bf20f61f53b8ce1
Tumbo Lobi John <johnlobiit@gmail.com>
2021-10-01 18:58:44 +0300

    Added mpesa xpress payment module

--- BetTip/urls.py
+++ BetTip/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from core import views
 from django.contrib.auth import views as auth_views
 from django.views.generic import TemplateView
+from mpesa.urls import mpesa_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,4 +27,5 @@ urlpatterns = [
     path('sign-out/', auth_views.LogoutView.as_view(next_page="/")),
     path('sign-up/', views.sign_up),
     path('accounts/profile/', views.profile_page, name="profile"),
+    path('mpesa/', include(mpesa_urls)),
 ]
